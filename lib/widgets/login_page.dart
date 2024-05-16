3d73eaa1574cc2dc74d5c9e5efc753cee0141af7
Omar Sanad <omarmohamedsanad3@gmail.com>
2024-05-16 23:10:02 +0300

    Added a background for the login page

--- lib/widgets/login_page.dart
+++ lib/widgets/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:creative_portsaid/widgets/sign_up_page.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -18,36 +19,53 @@ class _LoginPageState extends State<LoginPage> {
         onTap: () {
           FocusScope.of(context).unfocus();
         },
-        child: Center(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'lib/assets/creativa_logo.jpg',
-                  height: 100,
-                ),
-                const SizedBox(height: 20),
-                _buildTextField("Email", false, emailController),
-                const SizedBox(height: 20),
-                _buildTextField("Password", true, passwordController),
-                const SizedBox(height: 20),
-                _buildButton(
-                  "Login",
-                  () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const Placeholder()),
-                    );
-                  },
-                ),
-                const SizedBox(height: 20),
-                _buildAdditionalOptions(context),
-              ],
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset(
+              'lib/assets/creativa_building.jpeg',
+              fit: BoxFit.cover,
             ),
-          ),
+            BackdropFilter(
+              filter: ui.ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+              child: Container(
+                color: Colors.black
+                    .withOpacity(0.1), // Adjust the opacity if needed
+              ),
+            ),
+            Center(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'lib/assets/creativa_logo.jpg',
+                      height: 100,
+                    ),
+                    const SizedBox(height: 20),
+                    _buildTextField("Email", false, emailController),
+                    const SizedBox(height: 20),
+                    _buildTextField("Password", true, passwordController),
+                    const SizedBox(height: 20),
+                    _buildButton(
+                      "Login",
+                      () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const Placeholder(),
+                          ),
+                        );
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    _buildAdditionalOptions(context),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -57,11 +75,20 @@ class _LoginPageState extends State<LoginPage> {
 Widget _buildTextField(
     String label, bool obscureText, TextEditingController controller) {
   return TextField(
+    style: const TextStyle(color: Color(0xFF034C9F)),
     controller: controller,
     obscureText: obscureText,
     decoration: InputDecoration(
-      labelText: label,
-      focusColor: const Color(0xFF034C9F),
+      hintText: label,
+      filled: true,
+      fillColor: const Color(0x60EEEEEE),
+      focusColor: const Color(0x60EEEEEE),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(
+          color: Color(0x60EEEEEE),
+        ),
+      ),
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(10),
       ),
@@ -74,6 +101,14 @@ Widget _buildTextField(
 Widget _buildButton(String label, Function onPressed) {
   return ElevatedButton(
     onPressed: () => onPressed(),
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(
+        const Color(0x60EEEEEE),
+      ),
+      foregroundColor: MaterialStateProperty.all(
+        Colors.black,
+      ),
+    ),
     child: Text(label),
   );
 }
@@ -92,7 +127,10 @@ Widget _buildAdditionalOptions(BuildContext context) {
             ),
           );
         },
-        child: const Text("Sign Up"),
+        child: const Text(
+          "Sign Up",
+          style: TextStyle(color: Color(0x60EEEEEE)),
+        ),
       ),
     ],
   );
