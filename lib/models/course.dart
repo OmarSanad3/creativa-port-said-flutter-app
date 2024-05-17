e9e282ab4f002ea7b16751dd36f87dfa39e10c72
Omar Sanad <omarmohamedsanad3@gmail.com>
2024-05-17 18:33:05 +0300

    Created a class to represent each course

---
+++ lib/models/course.dart
@@ -0,0 +1,16 @@
+class Course {
+  Course({
+    required this.courseName,
+    required this.englishDescription,
+    required this.arabicDescription,
+    required this.duration,
+    required this.imagePath,
+    required this.formLink,
+  });
+  String courseName;
+  String englishDescription;
+  String arabicDescription;
+  int duration;
+  String imagePath;
+  String formLink;
+}
