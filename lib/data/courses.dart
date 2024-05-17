8dbed82685770081c0c722c687295edcd937c2d6
Omar Sanad <omarmohamedsanad3@gmail.com>
2024-05-17 18:33:30 +0300

    Added the courses to data folder

---
+++ lib/data/courses.dart
@@ -0,0 +1,155 @@
+import 'package:creative_portsaid/models/course.dart';
+
+List<Course> myCourses = [
+  Course(
+    courseName: "Ideation Bootcamp (Think & Design)",
+    englishDescription:
+        "is pivotal to the emergence of new startups, as it ignites creative thinking, and ensures a continuous flow of innovative ideas to strengthen the entrepreneurship ecosystem in our incubator or others",
+    arabicDescription:
+        "يعد تدريبا محورياً لظهور الشركات الناشئة الجديدة، حيث أنه يشعل التفكير الإبداعي، ويضمن التدفق المستمر للأفكار المبتكرة لتعزيز النظام البيئي لريادة الأعمال في حاضنتنا أو غيرها",
+    duration: 11,
+    imagePath: "",
+    formLink: "https://forms.gle/P5aRwGSimmLAkRnm9",
+  ),
+  Course(
+    courseName: "presentation & communication skills for entrepreneurs",
+    englishDescription:
+        "improve the skills of future entrepreneurs using capacity-building skills, presentation skills program is part of the startup journey program (pre-incubation program)",
+    arabicDescription:
+        "تحسين مهارات رواد الأعمال المستقبليين باستخدام مهارات بناء القدرات، ويعد برنامج مهارات العرض جزءًا من برنامج رحلة الشركات الناشئة",
+    duration: 2,
+    imagePath: "",
+    formLink: "https://forms.gle/TdU4uq2b23XX3k4P8",
+  ),
+  Course(
+    courseName: "Leadership skills for entrepreneurs",
+    englishDescription:
+        "improve the skills of future entrepreneurs using capacity-building skills, leadership program is part of the startup journey program (pre-incubation program)",
+    arabicDescription:
+        "تحسين مهارات رواد الأعمال المستقبليين باستخدام مهارات بناء القدرات، ويعد برنامج القيادة جزءًا من برنامج رحلة الشركات الناشئة",
+    duration: 2,
+    imagePath: "",
+    formLink: "https://forms.gle/UcrNpCSA24pjfx1J8",
+  ),
+  Course(
+    courseName: "Time management for entrepreneurs",
+    englishDescription:
+        "improve the skills of future entrepreneurs using capacity-building skills, Time management program is part of the startup journey program (pre-incubation program)",
+    arabicDescription:
+        "تحسين مهارات رواد الأعمال المستقبليين باستخدام مهارات بناء القدرات، برنامج إدارة الوقت جزء من برنامج رحلة الشركات الناشئة (برنامج ما قبل الحضانة)",
+    duration: 2,
+    imagePath: "",
+    formLink: "https://forms.gle/xBFxoLooLt8BZWse6",
+  ),
+  Course(
+    courseName: "Supply-chain workshop",
+    englishDescription:
+        "The Supply Chain Workshop is vital for startups graduating from our entrepreneurship training because it equips them with strategic tools to navigate complex market dynamics and build robust, scalable operations.",
+    arabicDescription:
+        "تعد ورشة عمل سلسلة التوريد أمرًا حيويًا للشركات الناشئة التي تتخرج من تدريبنا على ريادة الأعمال لأنها تزودهم بالأدوات الإستراتيجية للتنقل في ديناميكيات السوق المعقدة وبناء عمليات قوية وقابلة للتطوير.",
+    duration: 1,
+    imagePath: "",
+    formLink: "https://forms.gle/5FgwVZWLCxEav6YH9",
+  ),
+  Course(
+    courseName: "Career starter pack",
+    englishDescription:
+        "providing upcoming and recent graduates with the resources and guidance needed to begin their career journey (CV writing , Linkedin , interview skills and how to apply  for a job",
+    arabicDescription:
+        "تزويد الخريجين الجدد والقادمين بالموارد والإرشادات اللازمة لبدء رحلتهم المهنية (كتابة السيرة الذاتية، وLinkedin، ومهارات المقابلة، وكيفية التقدم للحصول على وظيفة)",
+    duration: 2,
+    imagePath: "",
+    formLink: "https://forms.gle/uzbmqKBEhCGVJyke9",
+  ),
+  Course(
+    courseName: "Freelancer factory",
+    englishDescription:
+        "basic and essential skills and knowledge required to start a freelancing career to get their first freelancing job on freelancing websites ( Flutter, web, and CS rounds and technical courses )already conducted will be targeted)",
+    arabicDescription:
+        "المهارات والمعرفة الأساسية والأساسية المطلوبة لبدء مهنة مستقلة للحصول على أول وظيفة مستقلة لهم على مواقع العمل الحر (سيتم استهداف جولات Flutter، والويب، وCS والدورات الفنية) التي تم إجراؤها بالفعل)",
+    duration: 2,
+    imagePath: "",
+    formLink: "https://forms.gle/dcUT5AGj5N1LoEGX7",
+  ),
+  Course(
+    courseName: "Flutter (mobile application)",
+    englishDescription:
+        "technical skills for freelancers and entrepreneurs on how to create your mobile application using Flutter to be uploaded on App store , Google Play and desktop",
+    arabicDescription:
+        "المهارات التقنية للعاملين المستقلينfreelancers  ورواد الأعمال حول كيفية إنشاء تطبيق الهاتف المحمول الخاص بك باستخدام Flutter ليتم تحميله على متجر التطبيقات وGoogle Play وسطح المكتب",
+    duration: 5,
+    imagePath: "",
+    formLink:
+        "https://forms.gle/v3PHWBQKQiiSMQXd9?fbclid=IwZXh0bgNhZW0CMTAAAR1ebIKmAT2afxS3ezy0qbSjZ4E77oROv9yDjXLhCO9jCKToQj8HJ5KhQmU_aem_AbxGhkXa5yzE3KX_kImvVvhsBss_lQnTIOlbsg-IhUILltO6OdKBQ1J4yli44YtaNy4SjFGAcltX8Ray1NH4TD04",
+  ),
+  Course(
+    courseName: "AI and machine learning fundamentals",
+    englishDescription:
+        "technical skills for freelancers and entrepreneurs on how to use AI methods into startups and gain profit for freelancers",
+    arabicDescription:
+        "المهارات التقنية للعاملين المستقلين ورواد الأعمال حول كيفية استخدام أساليب الذكاء الاصطناعي في الشركات الناشئة وتحقيق الربح للعاملين المستقلين",
+    duration: 5,
+    imagePath: "",
+    formLink: "https://forms.gle/ScyWLee9SGFVLhpm6",
+  ),
+  Course(
+    courseName: "cyber security fundamentals",
+    englishDescription:
+        "technical skills for freelancers and entrepreneurs on how to use cyber security into startups and gain profit for freelancers , forming the foundation of a secure professional and academic life.",
+    arabicDescription:
+        "المهارات التقنية للعاملين المستقلين ورواد الأعمال حول كيفية استخدام الأمن السيبراني في الشركات الناشئة وتحقيق الربح للعاملين لحسابهم الخاص، مما يشكل الأساس لحياة مهنية وأكاديمية آمنة.",
+    duration: 5,
+    imagePath: "",
+    formLink: "https://forms.gle/rHu39WLQ1ywQ5A4q7",
+  ),
+  Course(
+    courseName: "Web Development Fundamentals",
+    englishDescription:
+        "technical skills for freelancers and entrepreneurs on how to create your mobile application using create website (HTML , CSS , JS)",
+    arabicDescription:
+        "المهارات التقنية للعاملين المستقلين ورواد الأعمال حول كيفية إنشاء تطبيق الهاتف المحمول الخاص بك باستخدام إنشاء موقع ويب (HTML، CSS، JS)",
+    duration: 5,
+    imagePath: "",
+    formLink: "https://forms.gle/D2Wc3UHH4CGtvxd99",
+  ),
+  Course(
+    courseName: "Embedded Systems for beginners",
+    englishDescription:
+        "technical skills for freelancers and entrepreneurs on how to use Embedded system methods into startups and gain profit for freelancers",
+    arabicDescription:
+        "المهارات التقنية للعاملين المستقلين ورواد الأعمال حول كيفية استخدام أساليب النظام المضمن في الشركات الناشئة وتحقيق الربح للعاملين المستقلين",
+    duration: 5,
+    imagePath: "",
+    formLink: "https://forms.gle/B1k77vJLXsFAjwBm7",
+  ),
+  Course(
+    courseName: "Software Testing",
+    englishDescription:
+        "Offering a technology course on testing is essential to equip students and graduates with critical quality assurance skills, ensuring they can deliver reliable and robust tech solutions.",
+    arabicDescription:
+        "يعد تقديم دورة تقنية حول الاختبار أمرًا ضروريًا لتزويد الطلاب والخريجين بمهارات ضمان الجودة المهمة، مما يضمن قدرتهم على تقديم حلول تقنية موثوقة وقوية.",
+    duration: 5,
+    imagePath: "",
+    formLink: "https://forms.gle/RmbFhJ6c2VuskXaD6",
+  ),
+  Course(
+    courseName: "Power Bi Online course",
+    englishDescription:
+        "provided to students, graduates, and professionals to master data visualization, drive data-driven decision-making, and enhance business intelligence capabilities.",
+    arabicDescription:
+        "يعد تدريبا محوريًا للطلاب والخريجين والمهنيين لإتقان تصور البيانات، ودفع عملية صنع القرار المستندة إلى البيانات، وتعزيز قدرات ذكاء الأعمال.",
+    duration: 10,
+    imagePath: "",
+    formLink: "https://forms.gle/zhHzBqDxu9BPG2tZ9",
+  ),
+  Course(
+    courseName: "Pre-incubation program",
+    englishDescription:
+        "including business plan, marketing strategies ,Financial planning,  documentation , legal and HR , set of courses provided to ideation bootcamp graduates to support them in startup roadmap",
+    arabicDescription:
+        "بما في ذلك خطة العمل، واستراتيجيات التسويق، والتخطيط المالي، والتوثيق، والشؤون القانونية والموارد البشرية، ومجموعة من الدورات المقدمة لخريجي معسكرات التفكير لدعمهم في خارطة طريق بدء مشروعهم الخاص",
+    duration: 8,
+    imagePath: "",
+    formLink: "https://forms.gle/Yj7pQHLjszzn3EQM9",
+  ),
+];
