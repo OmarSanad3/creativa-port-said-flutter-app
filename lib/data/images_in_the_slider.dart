c3a279b0e939b55c42b7bfcdebc8d3c542e3ac9b
Omar Sanad <omarmohamedsanad3@gmail.com>
2024-06-02 16:09:20 +0300

    Added the data of the sliding images

---
+++ lib/data/images_in_the_slider.dart
@@ -0,0 +1,48 @@
+import 'package:creative_portsaid/models/sliding_images.dart';
+
+List<SlidingImages> imagesInTheSlider = [
+  SlidingImages(
+    imgPath: "lib/assets/sliding_assets/get_a_calm_place_to_study.jpeg",
+    caption: "Get a calm place to study",
+  ),
+  SlidingImages(
+    imgPath: "lib/assets/sliding_assets/get_evaluated_from_greate_minds.jpg",
+    caption: "Get evaluated from great minds",
+  ),
+  SlidingImages(
+    imgPath: "lib/assets/sliding_assets/get_high_quality_lessons.jpeg",
+    caption: "Get high quality courses",
+  ),
+  SlidingImages(
+    imgPath: "lib/assets/sliding_assets/get_out_of_your_comfort_zone.jpg",
+    caption: "Get out of your comfort zone",
+  ),
+  SlidingImages(
+    imgPath: "lib/assets/sliding_assets/learn_communication_skills.jpg",
+    caption: "Learn communication skills",
+  ),
+  SlidingImages(
+    imgPath: "lib/assets/sliding_assets/learn_new_skills.jpeg",
+    caption: "Learn new skills",
+  ),
+  SlidingImages(
+    imgPath: "lib/assets/sliding_assets/listen_to_impressive_people.jpeg",
+    caption: "Listen to impressive people",
+  ),
+  SlidingImages(
+    imgPath: "lib/assets/sliding_assets/meet_out_extroadinary_organizers.jpeg",
+    caption: "Meet out extraordinary organizers",
+  ),
+  SlidingImages(
+    imgPath: "lib/assets/sliding_assets/meet_wonderful_minds.jpg",
+    caption: "Meet wonderful minds",
+  ),
+  SlidingImages(
+    imgPath: "lib/assets/sliding_assets/push_your_boundaries.jpeg",
+    caption: "Push your boundaries",
+  ),
+  SlidingImages(
+    imgPath: "lib/assets/sliding_assets/work_in_teams.jpeg",
+    caption: "Work in teams",
+  ),
+];
